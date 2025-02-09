7d3c454b4c7bb6d8e6a73aaa29ce65a5d5365637
Arnaud <ngabgnaarnaud2@gmail.com>
2025-02-09 22:01:36 +0100

    ajout des screenshot à la fin de chaque test pour demonstration

--- cucumber/src/test/java/com/cucumbersaucedemo/Hooks/Hook.java
+++ cucumber/src/test/java/com/cucumbersaucedemo/Hooks/Hook.java
@@ -1,9 +1,15 @@
 package com.cucumbersaucedemo.Hooks;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
 import com.cucumbersaucedemo.Tools.WebDriverTools;
 
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 
 public class Hook {
 
@@ -14,6 +20,15 @@ public class Hook {
 
     @After
     public void DeleteConfiguration(){
-      //  WebDriverTools.tearDown();
+      WebDriverTools.tearDown();
+    }
+
+    @AfterStep
+    public void captureScreenshot(Scenario scenario){
+      if (!scenario.isFailed()) {  // Vérifie si le test a échoué
+            WebDriver driver = WebDriverTools.GetWebDriver();
+            byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", "Capture d'écran");
+        }
     }
 }
--- cucumber/src/test/resources/com/cucumbersaucedemo/product.feature
+++ cucumber/src/test/resources/com/cucumbersaucedemo/product.feature
@@ -6,14 +6,14 @@ Feature: Gestion des produits dans le panier sur SauceDemo
     Given l utilisateur est connecte avec "standard_user" et "secret_sauce" sur l url "https://www.saucedemo.com/"
     And il est sur la page des produits
 
-  @verification_panier
+  @smoke
   Scenario: Verification du prix hors panier et dans le panier
     When l utilisateur ajoute le premier produit au panier
     Then le badge du panier doit afficher "1"
     When il ouvre le panier
     Then le prix du produit dans le panier doit etre identique au prix affiche sur la page produit
 
-  @supprimer_panier
+  @smoke
   Scenario: Suppression d un article du panier
     When l utilisateur ajoute le premier produit au panier
     And il ouvre le panier
